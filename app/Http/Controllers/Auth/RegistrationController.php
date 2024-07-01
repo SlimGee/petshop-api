3bf5a42f2e389f21d24491ddde1da236be46f072
Given Ncube <givenyslim12@gmail.com>
2024-07-01 19:08:37 +0200

    fix: added form request to registraion

--- app/Http/Controllers/Auth/RegistrationController.php
+++ app/Http/Controllers/Auth/RegistrationController.php
@@ -3,27 +3,17 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\StoreUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules\Password;
 
 class RegistrationController extends Controller
 {
-    public function store(Request $request)
+    public function store(StoreUserRequest $request)
     {
-        $data = $request->validate([
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users',
-            'password' => ['required', 'confirmed', Password::defaults()],
-            'address' => 'required|string|max:255',
-            'avatar' => 'sometimes|nullable|uuid',
-            'phone_number' => 'required|phone:LENIENT',
-            'is_admin' => 'sometimes|nullable|boolean',
-        ]);
+        $data = $request->validated();
 
         $user = User::create([
             ...$data,
---
+++ app/Http/Requests/Auth/StoreUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => ['required', 'confirmed', Password::defaults()],
+            'address' => 'required|string|max:255',
+            'avatar' => 'sometimes|nullable|uuid',
+            'phone_number' => 'required|phone:LENIENT',
+            'is_admin' => 'sometimes|nullable|boolean',
+        ];
+    }
+}
